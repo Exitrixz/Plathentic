8d792a57d1a8205d786a1ed8eb6fa8f6c1790b4d
Exitrixz <145232163+Exitrixz@users.noreply.github.com>
2024-07-31 02:09:59 +0300

    Add files via upload

---
+++ 222extra-script.js
@@ -0,0 +1,35 @@
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = '#22241c';
+    document.getElementById('main-header1').style.backgroundColor = '#22341c';
+    document.getElementById('lobby').style.backgroundColor = '#76b876';
+    document.getElementById('about').style.backgroundColor = '#76b876';
+    document.getElementById('contact').style.backgroundColor = '#76b876';
+    document.getElementById('lobby').style.color = 'white';
+    document.getElementById('about').style.color = 'white';
+    document.getElementById('contact').style.color = 'white';
+    document.getElementById('D-L-M').style.backgroundColor = '#76b876';
+    document.getElementById('D-L-M2').style.backgroundColor = '#76b876';
+    document.getElementById('D-L-M').style.color = 'white';
+    document.getElementById('D-L-M2').style.color = 'white';
+    document.getElementById('filters1').style.color = 'white';
+    document.getElementById('main-title1').style.color = 'white';
+}
+
+        /*LIGHT MODE*/
+function changeBackgroundColor2() {
+    document.body.style.backgroundColor = 'rgb(248, 233, 206)';
+    document.getElementById('main-header1').style.color = 'black';
+    document.getElementById('main-header1').style.backgroundColor = 'rgb(220, 255, 221)';
+    document.getElementById('lobby').style.backgroundColor = '#58b94b';
+    document.getElementById('about').style.backgroundColor = '#58b94b';
+    document.getElementById('contact').style.backgroundColor = '#58b94b';
+    document.getElementById('lobby').style.color = 'black';
+    document.getElementById('about').style.color = 'black';
+    document.getElementById('contact').style.color = 'black';
+    document.getElementById('D-L-M').style.backgroundColor = '#58b94b';
+    document.getElementById('D-L-M2').style.backgroundColor = '#58b94b';
+    document.getElementById('D-L-M').style.color = 'black';
+    document.getElementById('D-L-M2').style.color = 'black';
+    document.getElementById('filters1').style.color = 'black';
+    document.getElementById('main-title1').style.color = 'black';
+}
